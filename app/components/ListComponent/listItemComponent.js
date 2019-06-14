9342d9bb08c805295ecaaa43fe2d1c0d0bd7a77a
shehand <sonodewcomplex@gmail.com>
2019-06-14 22:01:13 +0530

    Update list component view for new suggested UI

--- app/components/ListComponent/listItemComponent.js
+++ app/components/ListComponent/listItemComponent.js
@@ -1,33 +1,44 @@
 import React, { Component } from "react";
 import { View, Text, Image, TouchableOpacity } from "react-native";
 import styles from "./style";
-import { Rating } from "react-native-elements";
 
 export default class CardView extends Component {
   render() {
     return (
-      <View style={styles.cardContainer}>
+      <View style={styles.cardContainer} elevation={5}>
         <TouchableOpacity
           style={styles.cardHedear}
           onPress={this.props.onPress}
         >
           <View style={styles.userDetailArea}>
-            <View style={styles.userNameRow}>
-              <Text style={styles.routeNameText}>{this.props.posted}</Text>
-            </View>
-            <View style={{ flex: 0.25 }}>
+            <View style={{ flex: 0.2 }}>
               <Image
-                style={{ height: 130, width: 310, marginTop: 25 }}
+                style={styles.mapStyle}
                 source={require("../../images/landing.jpg")}
               />
             </View>
 
             <View style={styles.meaasageRow}>
-              <View style={styles.headerRow}>
-                <Text style={styles.nameText}>{this.props.name}</Text>
-                <Rating imageSize={15} startingValue={4} style={{marginLeft: 80}}/>
+              <View
+                style={{
+                  width: 115,
+                  height: 50,
+                  backgroundColor: "powderblue"
+                }}
+              >
+                <Text>{this.props.from}</Text>
+                <Text>{this.props.to}</Text>
               </View>
-              <Text style={styles.meaasageText}>{this.props.message}</Text>
+              <View
+                style={{ width: 115, height: 50, backgroundColor: "skyblue" }}
+              >
+                <Text>{this.props.posted}</Text>
+              </View>
+              <View
+                style={{ width: 115, height: 50, backgroundColor: "steelblue" }}
+              >
+                <Text>{this.props.userID}</Text>
+              </View>
             </View>
           </View>
         </TouchableOpacity>
