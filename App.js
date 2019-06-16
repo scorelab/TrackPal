57407be8785aa6498d8c12cc950d8132108e0dfd
shehand <sonodewcomplex@gmail.com>
2019-06-16 18:20:58 +0530

    Add route to onboarding view to the auth stack

--- App.js
+++ App.js
@@ -20,6 +20,7 @@ import SharingScreen from "./app/screens/SharingScreen/sharingScreen.js";
 import LandingScreen from "./app/screens/LandingScreen/landingScreen.js";
 import SignUpScreen from "./app/screens/SignUpScreen/signUpScreen.js";
 import ResetPasswordScreen from "./app/screens/ResetPasswordScreen/resetPasswordScreen.js";
+import OnboardScreen from "./app/screens/Onboarding/onboardingScreen.js"
 
 // bottom tab navigator to authed users
 const ScreenStack = createBottomTabNavigator(
@@ -58,43 +59,6 @@ const ScreenStack = createBottomTabNavigator(
   }
 );
 
-// bottom tab navigator to not authed users
-const nonAuthScreenStack = createBottomTabNavigator(
-  {
-    Bus: { screen: BusScreen },
-    Train: { screen: TrainScreen }
-  },
-  {
-    defaultNavigationOptions: ({ navigation }) => ({
-      tabBarIcon: ({ focused, tintColor }) => {
-        const { routeName } = navigation.state;
-        let iconName;
-        if (routeName === "Bus") {
-          iconName = "bus";
-        } else if (routeName === "Train") {
-          iconName = "train_";
-        }
-        return (
-          <CustomIcon
-            name={iconName}
-            size={25}
-            color={tintColor}
-            style={{
-              borderRadius: 50,
-              backgroundColor: "rgba(0,0,0,0.1115)",
-              padding: 10
-            }}
-          />
-        );
-      }
-    }),
-    tabBarOptions: {
-      activeTintColor: "#3d9bf9",
-      inactiveTintColor: "gray"
-    }
-  }
-);
-
 const AuthStack = createStackNavigator(
   {
     App: {
@@ -115,8 +79,8 @@ const AuthStack = createStackNavigator(
     ResetPassword: {
       screen: ResetPasswordScreen
     },
-    noAuthed: {
-      screen: nonAuthScreenStack
+    Onboard:{
+      screen: OnboardScreen
     }
   },
   {
