e90c7dd2dd6ca1c84e45fb92f755c019c227810f
shehand <sonodewcomplex@gmail.com>
2019-07-19 19:23:59 +0530

    Add secondary image and message to the onboarding slider

--- app/screens/Onboarding/onboardingScreen.js
+++ app/screens/Onboarding/onboardingScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { View, Text, ImageBackground } from "react-native";
+import { View, Text, ImageBackground, Image } from "react-native";
 import Onboarding from "react-native-onboarding-swiper";
 import styles from "./styles.js";
 
@@ -22,6 +22,16 @@ export default class OnboardingScreen extends Component {
                     style={styles.imageBackground}
                     source={require("../../images/bg-img-onboarding-1.png")}
                   >
+                    <Image
+                      style={styles.popMessageIcon}
+                      source={require("../../images/bell.png")}
+                    />
+                    <View style={styles.popMessageView}>
+                      <Text style={styles.popMessage}>
+                        Usual Traffic Report in {"\n"} your Route!
+                      </Text>
+                    </View>
+
                     <Text style={styles.textStyle_1}>Get to know</Text>
                     <Text style={styles.textStyle_2}>before you</Text>
                     <Text style={styles.textStyle_3}>Go</Text>
@@ -37,6 +47,32 @@ export default class OnboardingScreen extends Component {
                     style={styles.imageBackground}
                     source={require("../../images/bg-img-onboarding-2.png")}
                   >
+                    <Image
+                      style={{ marginTop: "60.5%", left: 75, zIndex: 1000 }}
+                      source={require("../../images/time.png")}
+                    />
+                    <View
+                      style={{
+                        position: "absolute", // child
+                        bottom: "60%", // position where you want
+                        left: 70
+                      }}
+                    >
+                      <Text
+                        style={{
+                          fontSize: 16,
+                          color: "black",
+                          paddingLeft: 50,
+                          fontFamily: "Product Sans",
+                          borderRadius: 50,
+                          backgroundColor: "white",
+                          width: 300,
+                          padding: 10
+                        }}
+                      >
+                        Your Train : සෙන්කඩගල මැණිකේ {"\n"} ETA 12 min
+                      </Text>
+                    </View>
                     <Text style={styles.textStyle_1}>Never Miss</Text>
                     <Text style={styles.textStyle_2}>your commute</Text>
                     <Text style={styles.textStyle_3}>Again</Text>
