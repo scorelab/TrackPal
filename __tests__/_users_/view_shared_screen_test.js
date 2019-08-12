915527aa14fbb2886ce4a7ae5da8ff60021e354c
shehand <sonodewcomplex@gmail.com>
2019-08-12 10:24:16 +0530

    Add test file to test shared screen

---
+++ __tests__/_users_/view_shared_screen_test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import SharingScreen from '../../app/screens/SharingScreen/sharingScreen.js';
+
+import renderer from 'react-test-renderer';
+
+jest.useFakeTimers();
+
+test('renders correctly', () => {
+  const tree = renderer.create(<SharingScreen/>).toJSON();
+  expect(tree).toMatchSnapshot();
+});
