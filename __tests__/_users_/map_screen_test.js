2d7a76990edad6c7af40ddf2b6caebfb21b69d6d
shehand <sonodewcomplex@gmail.com>
2019-08-18 18:29:34 +0530

    Update test mechanism

--- __tests__/_users_/map_screen_test.js
+++ __tests__/_users_/map_screen_test.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import MapScreen from '../../app/screens/MapScreen/mapScreen.js';
+import React from "react";
+import { Platform } from "react-native";
+import MapScreen from "../../app/screens/MapScreen/mapScreen.js";
 
-import renderer from 'react-test-renderer';
+import renderer from "react-test-renderer";
 
 jest.useFakeTimers();
+Platform.select = jest.fn(dict => dict.android);
 
-test('renders correctly', () => {
+test("renders correctly", () => {
   const tree = renderer.create(<MapScreen />).toJSON();
   expect(tree).toMatchSnapshot();
 });
