6a7cde34c24c789cfaa46182c908732703ae004f
ProCodec <60006102+ProCodec@users.noreply.github.com>
2020-01-23 23:22:18 +0530

    Add files via upload

---
+++ CodecOnline_UI/App.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { StyleSheet, Text, View ,ImageBackground} from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    const resizeMode = 'center';
+
+  return (
+    <ImageBackground  
+    style={{flex: 1, resizeMode,}}
+    source={require('./assets/bg.png')}
+    style={styles.container}>
+     </ImageBackground>
+    
+    );
+
+
+
+
+    return ([
+      
+    ]);
+} }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 110,
+    paddingLeft: 500,
+    width:-90,
+    height: '220%',
+    marginLeft:4,
+    
+    backgroundColor: '#fff',
+    
+    
+
+  },
+});
