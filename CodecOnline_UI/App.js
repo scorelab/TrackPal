0e3e1b958c894c0a2528d56a447db2d406269ce2
ProCodec <60006102+ProCodec@users.noreply.github.com>
2020-01-24 03:15:44 +0530

    UI

--- CodecOnline_UI/App.js
+++ CodecOnline_UI/App.js
@@ -1,39 +1,40 @@
 import React from 'react';
-import { StyleSheet, Text, View ,ImageBackground} from 'react-native';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     const resizeMode = 'center';
 
-  return (
-    <ImageBackground  
-    style={{flex: 1, resizeMode,}}
-    source={require('./assets/bg.png')}
-    style={styles.container}>
-     </ImageBackground>
-    
+    return (
+      <ImageBackground
+        style={{ flex: 1, resizeMode, }}
+        source={require('./assets/bg.gif')}
+        style={styles.container}>
+      </ImageBackground>
+
     );
 
 
 
 
     return ([
-      
+
     ]);
-} }
+  }
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 110,
     paddingLeft: 500,
-    width:-90,
+    width: -90,
     height: '220%',
-    marginLeft:4,
-    
+    marginLeft: 4,
+
     backgroundColor: '#fff',
-    
-    
+
+
 
   },
 });
